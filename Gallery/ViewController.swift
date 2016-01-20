a8483b86e179d93859d5c226ab16a539a152f96b
Michal Talaga <gomis71@gmail.com>
2016-01-20 19:12:18 +0100

    Initial Commit

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by Michał Talaga on 20.01.2016.
+//  Copyright © 2016 Michal Talaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
